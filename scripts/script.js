af54f5858391bbc6bf459cf2edb307645c234627
Ries <PS271589@summacollege.nl>
2026-05-19 14:29:12 +0200

    Add parties button and section to partijen page with styling and interactivity

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,11 @@ const startButton = document.getElementsByClassName('start-button')[0];
 const electionSelect = document.getElementsByClassName('election-select')[0];
 const loginButton = document.getElementsByClassName('login')[0];
 const registerButton = document.getElementsByClassName('register')[0];
+const partiesButton = document.getElementsByClassName('parties')[0];
+
+partiesButton.addEventListener('click', () => {
+    window.location.href = 'partijen.html';
+});
 
 loginButton.addEventListener('click', () => {
     window.location.href = 'login.php';
