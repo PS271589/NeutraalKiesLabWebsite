00ba03ecc1129a2f8f6177f795295df9c7777aec
Ries <PS271589@summacollege.nl>
2026-05-26 11:07:12 +0200

    Refactor login page; update layout, text, and styles for improved user experience

--- login.php
+++ login.php
@@ -9,28 +9,28 @@
     <link rel="icon" type="image/svg" href="assets/logo-neutraal-kieslab-lichtblauw.svg">
 </head>
 <body>
-    <div class="login-page">
+    <div class="register-page">
         <button class="back-button">
             <img src="assets/icon-back-arrow.svg" alt="Back arrow">
             Terug naar Home
         </button>
-        <div class="login-container">
+        <div class="register-container">
             <img class="container-logo" src="assets/logo-met-text-rechts.svg" alt="Neutraal KiesLab Logo">
-            <h1>Inloggen</h1>
-            <p>Log in op je account om je resultaten te bekijken</p>
+            <h1>Welkom terug</h1>
+            <p>Log in om je opgeslagen resultaten te bekijken</p>
             <form action="login.php" method="post">
                 <label for="email">E-mailadres</label>
                 <input type="email" id="email" name="email" placeholder="voorbeeld@email.nl" required><br>
 
-                <label for="password">Wachtwoord:</label>
-                <input type="password" id="password" name="password" placeholder="Minimaal 6 karakters" required><br>
+                <label for="password">Wachtwoord</label>
+                <input type="password" id="password" name="password" placeholder="••••••••" required><br>
 
                 <button type="submit" class="login-button">
                     <img src="assets/icon-login.svg" alt="Login icon">
                     Inloggen
                 </button>
             </form>
-            <p>Al een account? <a href="login.php">Log hier in</a></p>
+            <p>Nog geen account? <a href="register.php">Registreer je hier</a></p>
         </div>
     </div>
 </body>
